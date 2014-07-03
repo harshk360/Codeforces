4bb76afb0c4077eff0e0baf8d836eab14c4a1172
harshk360 <harshk360@gmail.com>
2014-07-04 00:00:17 +0530

    final

---
+++ Codeforces 236A/src/Codeforces 236A.cpp
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char s1[101];
+	char scanned[27];
+	for(int i=0;i<26;i++)
+		scanned[i]=0;
+	int sum=0;
+	scanf("%s", s1);
+	int length = strlen(s1);
+	for(int counter1=0;counter1<length;counter1++)
+	{
+		scanned[s1[counter1]-97]=1;
+	}
+	for(int counter2=0;counter2<26;counter2++)
+		{
+			sum+=scanned[counter2];
+		}
+	if(sum%2==0)
+		cout<<"CHAT WITH HER!";
+	else
+		cout<<"IGNORE HIM!";
+
+
+}
